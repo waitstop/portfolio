265da6f8fe6816b7150ca42307dcbc6596204803
waitstop <kazakov2003931@yandex.ru>
2024-11-11 06:21:51 +0300

    (feat), tailwind colors, font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "selector",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "rgb(var(--text))",
+        background: "rgb(var(--background))",
+        primary: "rgb(var(--primary))",
+        secondary: "rgb(var(--secondary))",
+        accent: "rgb(var(--accent))",
+      },
+      transitionTimingFunction: {
+        "out-back": "cubic-bezier(0.175, 0.885, 0.320, 1.275)",
+      },
+    },
   },
   plugins: [],
-};
+}
