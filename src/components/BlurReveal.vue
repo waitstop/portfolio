2adf311fea78e614c70d5dc1616ae1fb9c7e1224
waitstop <kazakov2003931@yandex.ru>
2024-11-11 17:25:28 +0300

    (feat), added BlurReaveal component

---
+++ src/components/BlurReveal.vue
@@ -0,0 +1,73 @@
+<template>
+  <div ref="container" :class="props.class">
+    <div
+      v-for="(child, index) in children"
+      :key="index"
+      ref="childElements"
+      v-motion
+      :initial="getInitial()"
+      :enter="getEnter(index)"
+    >
+      <component :is="child" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, useSlots, onMounted, watchEffect } from "vue"
+
+interface Props {
+  duration?: number
+  delay?: number
+  blur?: string
+  yOffset?: number
+  class?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  duration: 1,
+  delay: 2,
+  blur: "20px",
+  yOffset: 20,
+})
+
+const container = ref(null)
+const childElements = ref([])
+const slots = useSlots()
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const children = ref<any>([])
+
+onMounted(() => {
+  // This will reactively capture all content provided in the default slot
+  watchEffect(() => {
+    children.value = slots.default ? slots.default() : []
+  })
+})
+
+function getInitial() {
+  return {
+    opacity: 0,
+    filter: `blur(${props.blur})`,
+    y: props.yOffset,
+    transition: {
+      duration: 0,
+      easing: "easeInOut",
+      delay: 0,
+    },
+  }
+}
+
+function getEnter(index: number) {
+  return {
+    opacity: 1,
+    filter: `blur(0px)`,
+    y: 0,
+    transition: {
+      duration: props.duration * 1000,
+      easing: "easeInOut",
+      delay: props.delay * index * 1000,
+    },
+  }
+}
+</script>
