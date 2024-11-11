db4d2b0e0891b9676d02f3ae19803d7135a24806
waitstop <kazakov2003931@yandex.ru>
2024-11-11 06:20:08 +0300

    (build) iconify, radix-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import Components from "unplugin-vue-components/vite"
+import RadixVueResolver from "radix-vue/resolver"
+import { resolve } from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+      resolvers: [
+        RadixVueResolver(),
+        // Iconify
+        (name) => {
+          if (name === "Icon") return { name, from: "@iconify/vue" }
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: [{ find: "~", replacement: resolve(__dirname, "src") }],
+  },
 })
